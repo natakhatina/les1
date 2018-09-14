d3a146e06e80c18b54f8b2e02f68f3938f40e9b6
Наталья <natakhatina@gmail.com>
2018-09-14 12:11:01 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//class point;
+void print (int x,int y){
+    printf ("%d | %d",x,y);
+}
+
+int main() {
+    //std::cout << "Hello, World!" << std::endl;
+    int pointX=10;
+    int pointY=20;
+    print (pointX,pointY);
+    return 0;
+}
