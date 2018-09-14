01bf0a7f3fd2afb13fa0ec83634aabd81e74ada1
Наталья <natakhatina@gmail.com>
2018-09-14 12:35:46 +0300

    class

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 
-//class point;
-void print (int x,int y){
-    printf ("%d | %d",x,y);
-}
+class point{
+    int x;
+public:
+    void setX(int peremX){
+        x= peremX;
+    }
+    void print (){
+        printf("%d",x);
+    }
+    int giveX (int perem){
+        int a=2*perem;
+        return a;
+    }
+};
 
 int main() {
     //std::cout << "Hello, World!" << std::endl;
-    int pointX=10;
-    int pointY=20;
-    print (pointX,pointY);
+   point MyPoint;
+   MyPoint.setX(10);
+   int a=MyPoint.giveX(10);
+   printf("%d\n",a);
+   MyPoint.print();
     return 0;
 }
